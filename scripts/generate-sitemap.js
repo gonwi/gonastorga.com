649c961c86a905e586e63a949e2ebcf7157a5814
gonwi <gonzalo.astorgaie@gmail.com>
2021-01-20 22:19:40 +0800

    Updated sitemap

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -9,6 +9,7 @@ const prettier = require('prettier');
   // Ignore Next.js specific files (e.g., _app.js) and API routes.
   const pages = await globby([
     'pages/**/*{.js,.mdx}',
+    'posts/*.md',
     '!pages/_*.js',
     '!pages/api'
   ]);
