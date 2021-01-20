619211490c9df641f3e033d847676738ca547348
gonwi <gonzalo.astorgaie@gmail.com>
2021-01-20 22:24:25 +0800

    Fix generate sitemap

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -9,7 +9,7 @@ const prettier = require('prettier');
   // Ignore Next.js specific files (e.g., _app.js) and API routes.
   const pages = await globby([
     'pages/**/*{.js,.mdx}',
-    'posts/_*.md',
+    '!posts/*.md',
     '!pages/_*.js',
     '!pages/api'
   ]);
