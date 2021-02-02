aa4bd214f9164799b829fc495fd7066c7343e6e3
gonwi <gonzalo.astorgaie@gmail.com>
2021-02-02 20:09:21 +0800

    Create events.js

---
+++ data/events.js
@@ -0,0 +1,8 @@
+export default = [{name: 'BlockMad',
+                    city:'Madrid',
+                    address 'Google Campus'
+                    time: '18:30',
+                    url: 'https://',
+                    photos: ' '},
+                {}
+                ]
